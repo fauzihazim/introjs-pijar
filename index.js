ae1ec0d96610ce54b415336ff6760919da92b837
Fauzi Hazim <>
2022-11-25 12:12:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,249 @@
+// NOMOR 1 —--------------------------
+console.log("// NOMOR 1 —--------------------------");
+
+const namaLengkap = "Fauzi Hazim Wibowo";
+let email = "fauzihazim28@gmail.com";
+let nomorHandphone = "085701781650";
+let umur = 21;
+let ttl = "Sukoharjo, 28 Agustus 2001";
+let alamat = "Brajan Rt.01 Rw.02 Bugel Kecamatan Polokarto";
+let kota = "Sukoharjo";
+let provinsi = "Jawa Tengah";
+let kodePos = 57555;
+let programmingExperience = ["golang", "c++", "html", "css", "js"];
+let riwayatPendidikan = [{nama:"SDI Diponegoro", tingkat:"SD", tahunMulai:2007, tahunBerakhir:2013, jurusan:null, rekomendasi:true, lokasi:"Surakarta, Jawa Tengah", pengajarFavorit:["Fatimah", "Laila"], materiFavorit:[{nama:"SKI", date:2010, pengajar:"Fatimah", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Keswati", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Akhlaq", date:2010, pengajar:"Laila", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},
+                        
+{nama:"SMP N 3 Sukoharjo", tingkat:"SMP", tahunMulai:2013, tahunBerakhir:2016, jurusan:null, rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Lilik", "Saptono"], materiFavorit:[{nama:"Bahasa Inggris", date:2017, pengajar:"Saptono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Sri Muryani", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2010, pengajar:"Lilik", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},
+
+{nama:"SMA N 1 Sukoharjo", tingkat:"SMA", tahunMulai:2016, tahunBerakhir:2019, jurusan:"IPA", rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Suranto", "Umi"], materiFavorit:[{nama:"Bahasa Inggris", date:2019, pengajar:"Mulyono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2019, pengajar:"Suranto", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Geografi", date:2019, pengajar:"Umi", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},
+
+{nama:"Universitas Telkom", tingkat:"Universitas", tahunMulai:2019, tahunBerakhir:2023, jurusan:"S1 Informatika", rekomendasi:true, lokasi:"Bandung, Jawa Barat", pengajarFavorit:["Stevanie", "Agung"], materiFavorit:[{nama:"DAP", date:2019, pengajar:"Stevanie", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"BK", date:2019, pengajar:"Agung", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2019, pengajar:"Ahmad", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false},]},];
+
+let golonganDarah = "B";
+let hobi = ["Koding", "Memasak", "Nongkrong"];
+let makananFavorit = [{nama:"Magelangan", asal:"Indonesia"}, {nama:"Soto", asal:"Indonesia"}, {nama:"Ayam Geprek", asal:"Indonesia"}];
+
+// NOMOR 2 —--------------------------
+console.log("// NOMOR 2 —--------------------------");
+let FauziHazimWibowo = {namaLengkap: "Fauzi Hazim Wibowo", email: "fauzihazim28@gmail.com", nomorHandphone: "085701781650", umur: 21, ttl: "Sukoharjo, 28 Agustus 2001", alamat: "Brajan Rt.01 Rw.02 Bugel Kecamatan Polokarto", kota: "Sukoharjo", provinsi: "Jawa Tengah", kodePos: 57555, programmingExperience: ["golang", "c++", "html", "css", "js"],
+riwayatPendidikan:[{nama:"SDI Diponegoro", tingkat:"SD", tahunMulai:2007, tahunBerakhir:2013, jurusan:null, rekomendasi:true, lokasi:"Surakarta, Jawa Tengah", pengajarFavorit:["Fatimah", "Laila"], materiFavorit:[{nama:"SKI", date:2010, pengajar:"Fatimah", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Keswati", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Akhlaq", date:2010, pengajar:"Laila", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},
+{nama:"SMP N 3 Sukoharjo", tingkat:"SMP", tahunMulai:2013, tahunBerakhir:2016, jurusan:null, rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Lilik", "Saptono"], materiFavorit:[{nama:"Bahasa Inggris", date:2017, pengajar:"Saptono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Sri Muryani", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2010, pengajar:"Lilik", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},{nama:"SMA N 1 Sukoharjo", tingkat:"SMA", tahunMulai:2016, tahunBerakhir:2019, jurusan:"IPA", rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Suranto", "Umi"], materiFavorit:[{nama:"Bahasa Inggris", date:2019, pengajar:"Mulyono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2019, pengajar:"Suranto", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Geografi", date:2019, pengajar:"Umi", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]}, {nama:"Universitas Telkom", tingkat:"Universitas", tahunMulai:2019, tahunBerakhir:2023, jurusan:"S1 Informatika", rekomendasi:true, lokasi:"Bandung, Jawa Barat", pengajarFavorit:["Stevanie", "Agung"], materiFavorit:[{nama:"DAP", date:2019, pengajar:"Stevanie", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"BK", date:2019, pengajar:"Agung", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2019, pengajar:"Ahmad", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false},]},],
+golonganDarah:"B",
+hobi:["Koding", "Memasak", "Nongkrong"],
+makananFavorit:[{nama:"Magelangan", asal:"Indonesia"}, {nama:"Soto", asal:"Indonesia"}, {nama:"Ayam Geprek", asal:"Indonesia"}],};
+
+// NOMOR 3 —--------------------------
+console.log("// NOMOR 3 —--------------------------");
+console.log(FauziHazimWibowo.namaLengkap);
+console.log(FauziHazimWibowo.nomorHandphone);
+console.log(FauziHazimWibowo.programmingExperience);
+console.log(Object.values(FauziHazimWibowo.makananFavorit[0]));
+console.log(FauziHazimWibowo.hobi[2]);
+console.log(FauziHazimWibowo.riwayatPendidikan[3].materiFavorit[2].nama + ", " + FauziHazimWibowo.riwayatPendidikan[3].materiFavorit[2].pengajar);
+
+// NOMOR 4 —--------------------------
+console.log("// NOMOR 4 —--------------------------");
+let myFunction = (nama, pendidikan) => {
+  console.log(nama);
+  let i = 0;
+  // console.log(pendidikan.length);
+  while (i < pendidikan.length) {
+    console.log("Pernah menempuh pendidikan di " + pendidikan[i].nama + " tempat");
+    i++;
+  }
+}
+
+console.log(myFunction(FauziHazimWibowo.namaLengkap, FauziHazimWibowo.riwayatPendidikan));
+
+// NOMOR 5 —--------------------------
+console.log("// NOMOR 5 —--------------------------");
+let function5 = (num) => {
+  let i = 0;
+  while (i < num) {
+    console.log("Looping ke " + (i+1))
+    i++;
+  }
+}
+
+function5(5);
+
+// NOMOR 6 —--------------------------
+console.log("// NOMOR 6 —--------------------------");
+let printHobbies = (hobby) => {
+  let i = 0;
+  while (i < hobby.length) {
+    console.log("Hobi ke " + (i+1) +" adalah " + hobby[i]);
+    i++;
+  }
+}
+
+printHobbies(FauziHazimWibowo.hobi);
+
+// NOMOR 7 —--------------------------
+console.log("// NOMOR 7 —--------------------------");
+let checkPassingGrade = (grade) => {
+  if (grade >= 75) {
+    return "Selamat, anda lolos!";
+  } else {
+    return "Anda tidak lolos!";
+  }
+}
+let gradeCheckMessage = checkPassingGrade(76);
+console.log(gradeCheckMessage);
+
+// NOMOR 8 —--------------------------
+console.log("// NOMOR 8 —--------------------------");
+let printOddNumber = (num) => {
+  let i = 0;
+  while (i <= num) {
+    if (i % 2 != 0) {
+      console.log(i);
+    }
+    i++;
+  }
+}
+printOddNumber(5);
+
+// NOMOR 9 —--------------------------
+console.log("// NOMOR 9 —--------------------------");
+let printPijarCamp = (num) => {
+  if ((num % 3 == 0) && (num % 5 == 0)) {
+    return "PijarCamp";
+  } else if (num % 3 == 0) {
+    return "Pijar";
+  } else if (num % 5 == 0) {
+    return "Camp";
+  } else {
+    return num;
+  }
+}
+console.log(printPijarCamp(31));
+
+// NOMOR 10 —--------------------------
+console.log("// NOMOR 10 —--------------------------");
+let combineArray = (arr1, arr2) => {
+  return [...arr1,...arr2];
+}
+let combineObjects = (obj1, obj2) => {
+  return {...obj1,...obj2};
+}
+
+let arr1 = ["sukses", "karir", "cemerlang"];
+let arr2 = ["tawakal", "bersyukur", "berusaha"];
+console.log(combineArray(arr1, arr2));
+
+let obj1 = {namaLengkap : "Fauzi Hazim Wibowo", nama : "Fauzi", umur : 21};
+let obj2 = {citaCita : "Fullstack Web Dev", hobi : "Mengeluh", motto : "Cap Mobil"};
+console.log(combineObjects(obj1, obj2));
+
+// NOMOR 11 —--------------------------
+console.log("// NOMOR 11 —--------------------------");
+let storedObjects = (key, value) => {
+  let obj = {};
+  obj[key] = value;
+  return obj;
+}
+
+let obj = storedObjects("nama","Pratama");
+obj = {...obj, ...storedObjects("kota","Jakarta")};
+console.log(obj);
+
+// NOMOR 12 —--------------------------
+console.log("// NOMOR12 —--------------------------");
+FauziHazimWibowo = {
+namaLengkap: "Fauzi Hazim Wibowo", email: "fauzihazim28@gmail.com", nomorHandphone: "085701781650", umur: 21, ttl: "Sukoharjo, 28 Agustus 2001", alamat: "Brajan Rt.01 Rw.02 Bugel Kecamatan Polokarto", kota: "Sukoharjo", provinsi: "Jawa Tengah", kodePos: 57555, programmingExperience: ["golang", "c++", "html", "css", "js"],
+riwayatPendidikan:[{nama:"SDI Diponegoro", tingkat:"SD", tahunMulai:2007, tahunBerakhir:2013, jurusan:null, rekomendasi:true, lokasi:"Surakarta, Jawa Tengah", pengajarFavorit:["Fatimah", "Laila"], materiFavorit:[{nama:"SKI", date:2010, pengajar:"Fatimah", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Keswati", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Akhlaq", date:2010, pengajar:"Laila", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},
+{nama:"SMP N 3 Sukoharjo", tingkat:"SMP", tahunMulai:2013, tahunBerakhir:2016, jurusan:null, rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Lilik", "Saptono"], materiFavorit:[{nama:"Bahasa Inggris", date:2017, pengajar:"Saptono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2010, pengajar:"Sri Muryani", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2010, pengajar:"Lilik", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]},{nama:"SMA N 1 Sukoharjo", tingkat:"SMA", tahunMulai:2016, tahunBerakhir:2019, jurusan:"IPA", rekomendasi:true, lokasi:"Sukoharjo, Jawa Tengah", pengajarFavorit:["Suranto", "Umi"], materiFavorit:[{nama:"Bahasa Inggris", date:2019, pengajar:"Mulyono", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Matematika", date:2019, pengajar:"Suranto", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false}, {nama:"Geografi", date:2019, pengajar:"Umi", alasan:["Gurunya Seru", "Materinya menarik"], sulit:false},]}, {nama:"Universitas Telkom", tingkat:"Universitas", tahunMulai:2019, tahunBerakhir:2023, jurusan:"S1 Informatika", rekomendasi:true, lokasi:"Bandung, Jawa Barat", pengajarFavorit:["Stevanie", "Agung"], materiFavorit:[{nama:"DAP", date:2019, pengajar:"Stevanie", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"BK", date:2019, pengajar:"Agung", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false}, {nama:"Agama Islam", date:2019, pengajar:"Ahmad", alasan:["Dosennya Seru", "Materinya menarik"], sulit:false},]},],
+golonganDarah:"B",
+hobi:["Koding", "Memasak", "Nongkrong"],
+makananFavorit:[{nama:"Magelangan", asal:"Indonesia"}, {nama:"Soto", asal:"Indonesia"}, {nama:"Ayam Geprek", asal:"Indonesia"}],
+                       
+printNama : function() {
+  return this.namaLengkap;
+},
+
+printEmail : function() {
+  return this.email;
+},
+
+printPhoneNumber : function() {
+  return this.nomorHandphone;
+},
+};
+
+console.log(FauziHazimWibowo.printNama());
+console.log(FauziHazimWibowo.printEmail());
+console.log(FauziHazimWibowo.printPhoneNumber());
+
+// NOMOR 13 —--------------------------
+console.log("// NOMOR13 —--------------------------");
+let returnFirstName = (fullName) => fullName.split(" ", 1);
+
+let returnDay = (clock) => {
+  if ((5 <= clock) && (clock <= 12)) {
+    return "Pagi";
+  } else if ((12 <= clock) && (clock <= 15)) {
+    return "Siang";
+  } else if ((15 <= clock) && (clock <= 18)) {
+    return "Sore";
+  } else if (((18 <= clock) && (clock <= 24)) || ((0 <= clock) && (clock <= 5))) {
+    return "Malam";
+  }
+}
+
+let greeting = (clock, functionReturnDay, fullName, functionReturnFirstName) => "Selamat " + functionReturnDay(clock) + ", " + functionReturnFirstName(fullName);
+
+console.log(greeting(5, returnDay, "Fauzi Hazim Wibowo", returnFirstName));
+
+// NOMOR 14 —--------------------------
+console.log("// NOMOR14 —--------------------------");
+let printSquare = (num) => {
+  let i = 0;
+  while (i < num) {
+    let j = 0;
+    let content = "";
+    while (j < num) {
+      content += "* ";
+      j++;
+    }
+    console.log(content);
+    i++;
+  }
+}
+printSquare(5);
+
+// NOMOR 15 —--------------------------
+console.log("// NOMOR15 —--------------------------");
+let printTriangleWithNumber = (num) => {
+  try {
+    var success = true;
+    try {
+      if(typeof num != "number") throw "Parameter harus angka!";
+      if(num == "") throw "Parameter harus angka!";
+      if(isNaN(num)) throw "Parameter harus angka!";
+    } catch(err) {
+      console.log(err);
+    }
+    if(typeof num == "number") {
+      while (num > 0) {
+        let i = 0;
+        let content = "";
+        while (i < num) {
+          content += (i+1);
+          i++;
+        }
+        console.log(content);
+        num--;
+      }
+    }
+  } 
+  finally {
+    // console.log("Selesai");
+  }
+}
+printTriangleWithNumber(5);
+printTriangleWithNumber(3);
+printTriangleWithNumber("7");
+printTriangleWithNumber("tiga");
+printTriangleWithNumber([]);
+printTriangleWithNumber(NaN);
